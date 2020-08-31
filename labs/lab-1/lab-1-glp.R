40b74962be83a148eab1e5c8613425561fafb09e
Greg Pearson <greg.pearson.0161@gmail.com>
2020-08-31 10:28:18 -0400

    add files from week 1 and new file for week 2 labs to github

---
+++ labs/lab-1/lab-1-glp.R
@@ -0,0 +1,26 @@
+### Exercise 1
+
+x <- 3
+y <- 15
+x + y # sum
+x - y # difference
+x * y # product
+x / y # quotient
+
+### Exercise 2
+
+x <- 2
+y <- 3
+x^5 > y^4
+
+### Exercise 3
+
+d <- c(22, 62, 148, 43, 129)
+sum(d)
+sum(d)/d
+
+### Exercise 4
+n <- c("A","T","C","G")
+sort(n)
+
+
